3b7a51b3c72ad716e7780df695e50b0ad75416bb
rotemmi <68027682+rotemmi@users.noreply.github.com>
2021-07-11 15:06:27 +0300

    maintanability

--- FacebookWinFormsApp/MainForm.Designer.cs
+++ FacebookWinFormsApp/MainForm.Designer.cs
@@ -43,6 +43,14 @@
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
             this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.AppTab.SuspendLayout();
             this.myProfileTab.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.profilePicture)).BeginInit();
@@ -57,7 +65,7 @@
             this.AppTab.Controls.Add(this.tabPage3);
             this.AppTab.Controls.Add(this.tabPage4);
             this.AppTab.Location = new System.Drawing.Point(16, 15);
-            this.AppTab.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.AppTab.Margin = new System.Windows.Forms.Padding(4);
             this.AppTab.Name = "AppTab";
             this.AppTab.SelectedIndex = 0;
             this.AppTab.Size = new System.Drawing.Size(1035, 511);
@@ -66,13 +74,21 @@
             // myProfileTab
             // 
             this.myProfileTab.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.myProfileTab.Controls.Add(this.label5);
+            this.myProfileTab.Controls.Add(this.label6);
+            this.myProfileTab.Controls.Add(this.label7);
+            this.myProfileTab.Controls.Add(this.label8);
+            this.myProfileTab.Controls.Add(this.label3);
+            this.myProfileTab.Controls.Add(this.label4);
+            this.myProfileTab.Controls.Add(this.label2);
+            this.myProfileTab.Controls.Add(this.label1);
             this.myProfileTab.Controls.Add(this.logOutButton);
             this.myProfileTab.Controls.Add(this.listBoxFriendsList);
             this.myProfileTab.Controls.Add(this.profilePicture);
             this.myProfileTab.Location = new System.Drawing.Point(4, 25);
-            this.myProfileTab.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.myProfileTab.Margin = new System.Windows.Forms.Padding(4);
             this.myProfileTab.Name = "myProfileTab";
-            this.myProfileTab.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.myProfileTab.Padding = new System.Windows.Forms.Padding(4);
             this.myProfileTab.Size = new System.Drawing.Size(1027, 482);
             this.myProfileTab.TabIndex = 0;
             this.myProfileTab.Text = "My Profile";
@@ -80,30 +96,31 @@
             // 
             // logOutButton
             // 
-            this.logOutButton.Location = new System.Drawing.Point(271, 430);
-            this.logOutButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.logOutButton.Location = new System.Drawing.Point(832, 0);
+            this.logOutButton.Margin = new System.Windows.Forms.Padding(4);
             this.logOutButton.Name = "logOutButton";
-            this.logOutButton.Size = new System.Drawing.Size(244, 28);
+            this.logOutButton.Size = new System.Drawing.Size(195, 37);
             this.logOutButton.TabIndex = 3;
             this.logOutButton.Text = "Log Out";
             this.logOutButton.UseVisualStyleBackColor = true;
             this.logOutButton.Click += new System.EventHandler(this.logOutButton_Click);
             // 
-            // friendsList
+            // listBoxFriendsList
             // 
             this.listBoxFriendsList.FormattingEnabled = true;
             this.listBoxFriendsList.ItemHeight = 16;
-            this.listBoxFriendsList.Location = new System.Drawing.Point(808, 25);
-            this.listBoxFriendsList.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.listBoxFriendsList.Size = new System.Drawing.Size(159, 340);
+            this.listBoxFriendsList.Location = new System.Drawing.Point(240, 288);
+            this.listBoxFriendsList.Margin = new System.Windows.Forms.Padding(4);
+            this.listBoxFriendsList.Name = "listBoxFriendsList";
+            this.listBoxFriendsList.Size = new System.Drawing.Size(571, 116);
             this.listBoxFriendsList.TabIndex = 2;
             // 
             // profilePicture
             // 
-            this.profilePicture.Location = new System.Drawing.Point(245, 7);
-            this.profilePicture.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.profilePicture.Location = new System.Drawing.Point(4, 8);
+            this.profilePicture.Margin = new System.Windows.Forms.Padding(4);
             this.profilePicture.Name = "profilePicture";
-            this.profilePicture.Size = new System.Drawing.Size(383, 276);
+            this.profilePicture.Size = new System.Drawing.Size(152, 138);
             this.profilePicture.TabIndex = 0;
             this.profilePicture.TabStop = false;
             // 
@@ -112,9 +129,9 @@
             this.tabPage2.Controls.Add(this.button1);
             this.tabPage2.Controls.Add(this.textBox1);
             this.tabPage2.Location = new System.Drawing.Point(4, 25);
-            this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4);
             this.tabPage2.Size = new System.Drawing.Size(1027, 482);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
@@ -123,7 +140,7 @@
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(812, 151);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 1;
@@ -133,7 +150,7 @@
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(263, 151);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(132, 22);
             this.textBox1.TabIndex = 0;
@@ -141,7 +158,7 @@
             // tabPage3
             // 
             this.tabPage3.Location = new System.Drawing.Point(4, 25);
-            this.tabPage3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(4);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Size = new System.Drawing.Size(1027, 482);
             this.tabPage3.TabIndex = 2;
@@ -152,7 +169,7 @@
             // 
             this.tabPage4.Controls.Add(this.button4);
             this.tabPage4.Location = new System.Drawing.Point(4, 25);
-            this.tabPage4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage4.Margin = new System.Windows.Forms.Padding(4);
             this.tabPage4.Name = "tabPage4";
             this.tabPage4.Size = new System.Drawing.Size(1027, 482);
             this.tabPage4.TabIndex = 3;
@@ -162,12 +179,84 @@
             // button4
             // 
             this.button4.Location = new System.Drawing.Point(29, 320);
-            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(100, 28);
             this.button4.TabIndex = 0;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "First Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 260);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 377);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(21, 348);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(21, 317);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 17);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(21, 288);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 17);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "label8";
             // 
             // MainForm
             // 
@@ -175,10 +264,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.AppTab);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MainForm";
             this.AppTab.ResumeLayout(false);
             this.myProfileTab.ResumeLayout(false);
+            this.myProfileTab.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.profilePicture)).EndInit();
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
@@ -200,5 +290,13 @@
         private System.Windows.Forms.ListBox listBoxFriendsList;
         private System.Windows.Forms.PictureBox profilePicture;
         private System.Windows.Forms.Button logOutButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
